a281980f19c92447abd20dac1d60a4c6763eeac7
Skezza <36114463+SkezzaB@users.noreply.github.com>
2020-07-30 20:03:21 +0100

    Included install_requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
-import setuptools
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+    
+requirements = []
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
 
-setuptools.setup(
+setup(
     name="Py2DF", # Replace with your own username
     version="0.0.1",
     author="PgBiel, Skezza",
@@ -13,6 +17,7 @@ setuptools.setup(
     description="A tool to convert python scripts to DF code templates.",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    install_requires=requirements,
     url="https://github.com/Py2DF/Py2DF",
     packages=setuptools.find_packages(),
     classifiers=[
