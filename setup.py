7a12b753cd1eca59d4391d420d28e67fd9831d4c
Skezza <36114463+SkezzaB@users.noreply.github.com>
2020-08-16 20:21:00 +0100

    Oops, this lib doesn't support 3.6 due to nbtlib

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: Internet',
