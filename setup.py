312e1a240861480b759d4c03acf27d788982d0c4
Skezza <36114463+SkezzaB@users.noreply.github.com>
2020-08-18 13:33:01 +0100

    Revert

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6,
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: Internet',
